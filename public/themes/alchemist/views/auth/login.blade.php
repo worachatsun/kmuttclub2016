5725c94aa4a9e003b8b5252bbb87bdc81e3f94b5
KANISORN <firstziiz.k@gmail.com>
2016-08-22 01:33:36 +0700

    remove beta tag

--- public/themes/alchemist/views/auth/login.blade.php
+++ public/themes/alchemist/views/auth/login.blade.php
@@ -2,11 +2,6 @@
     <img src="<?php echo url(""); ?>/themes/alchemist/assets/img/logo-big-white.png" alt="" width="200">
 </div>
 
-<div class="alert alert-danger text-center">
-	<strong>BETA!</strong> Testing<br>
-	<i>Server : <?php echo gethostname(); ?>
-</div>
-
 <div class="content">
   @if ($errors->has())
     <div class="alert alert-danger">
