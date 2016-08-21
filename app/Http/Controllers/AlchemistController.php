8d2c4380b2f719b0d2a425e9cd3e19ef735d53e3
worachatsun <worachat.sun@mail.kmutt.ac.th>
2016-08-21 18:51:27 +0700

    renovate login

--- app/Http/Controllers/AlchemistController.php
+++ app/Http/Controllers/AlchemistController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use Input;
 use App\Repositories\AlchemistRepositoryInterface;
 
 class AlchemistController extends ACMBaseController{
     protected $AlchemistRepository;
 
     public function __construct(AlchemistRepositoryInterface $AlchemistRepository){
-        parent::__construct();
-        $this->AlchemistRepository = $AlchemistRepository;
+      parent::__construct();
+      $this->AlchemistRepository = $AlchemistRepository;
     }
 
     public function getIndex(){
-      
+      return redirect('alchemist/bindclub');
+    }
+
+    public function getBindclub(){
+      return $this->theme->scope('alchemist')->render();
+    }
+
+    public function postBindclub(){
+      $data = Input::all();
+      dd($data);
     }
 }
