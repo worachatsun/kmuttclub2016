ab4382fe30e88f53ff6a353fd94e7c09c2a5ec87
zecereal <zcrltv@gmail.com>
2016-08-21 18:46:05 +0700

    add switch dashboard

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -33,8 +33,10 @@ class StudentController extends ACMBaseController
 
         $std_id = array_get($this->user,'username');
         $clubs = $this->StudentRepository->getAllClubs($std_id);
+        $role = $this->StudentRepository->getRole($std_id);
         $content = array(
             'clubs' => $clubs,
+            'role' => $role
         );
         return $this->theme->layout('std')->scope('student.dashboard',$content)->render();
     }
@@ -45,6 +47,11 @@ class StudentController extends ACMBaseController
         $std_id = array_get($this->user,'username');
         $this->StudentRepository->deleteMyClub($std_id,$club_id);
         return redirect('student/dashboard');
+    }
+
+    public function postClubDashboard()
+    {
+        
     }
 
 
